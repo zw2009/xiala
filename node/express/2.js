dc50a1156ff10225cd2a46dc2573fe63691eee33
281025145@qq.com <281025145@qq.com>
2019-06-28 07:28:19 +0800

    node

--- node/express/2.js
+++ node/express/2.js
@@ -4,11 +4,30 @@ let app = express();
 //主页输出‘hello world
 app.get('/',function(req,res){
  	console.log('主页get 请求');
- 	res.send('Hello GRT');
+ 	res.send('Hello GET');
 });
 
 //post请求
 app.post('/',function(req,res){
 	console.log("主页post请求");
-	res.send('')
-})
+	res.send('Hello POST');
+});
+
+//del_user 页面相应 GET 请求
+app.get('/list_user',function(req,res){
+	console.log('/list_user GET 请求');
+	res.send();
+});
+
+//对页面abcd ， abxcd ， ab123cd ， 等响应 GET 请求
+app.get('/ab*cd',function(req,res){
+	console.log("/ab*cd GET 请求");
+	res.send("正则匹配");
+});
+
+var server = app.listen(8081,function(){
+	var host = server.address().host;
+	var port = server.address().port;
+	
+	console.log("应用实例，访问地址为 http://%s%s",host,port)
+});
